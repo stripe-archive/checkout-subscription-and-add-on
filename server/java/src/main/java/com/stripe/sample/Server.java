d888c1644d1bd35f6b6558afb8bbad9d5e852e85
Adrienne Dreyfus <adreyfus@stripe.com>
2019-11-04 09:25:28 -0800

    Updated README

--- server/java/src/main/java/com/stripe/sample/Server.java
+++ server/java/src/main/java/com/stripe/sample/Server.java
@@ -42,7 +42,7 @@ public class Server {
 
     public static void main(String[] args) {
         port(4242);
-        String ENV_PATH = "../../";
+        
         Dotenv dotenv = Dotenv.load();
 
         Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
