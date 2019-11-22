6b43577c6ab851dc2a5642f2025ce54b088c4aa2
Andrew Bonventre <andybons@gmail.com>
2019-11-21 23:21:45 -0500

    Add Go server example

--- server/README.md
+++ server/README.md
@@ -1,6 +1,6 @@
 # Running the server
 
-We included several RESTful server that each implement the same endpoints and logic. 
+We included several RESTful server that each implement the same endpoints and logic.
 Pick the language you are most comfortable in and follow the instructions in the directory on how to run.
 
 # Supported languages
@@ -10,3 +10,4 @@ Pick the language you are most comfortable in and follow the instructions in the
 * [Ruby (Sinatra)](ruby/README.md)
 * [PHP (Slim)](php/README.md)
 * [Java (Spark)](java/README.md)
+* [Go (Standard Library)](go/README.md)
---
+++ server/go/server.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/checkout/session"
+	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/webhook"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("godotenv.Load: %v", err)
+	}
+
+	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+
+	http.Handle("/", http.FileServer(http.Dir(os.Getenv("STATIC_DIR"))))
+	http.HandleFunc("/create-checkout-session", handleCreateCheckoutSession)
+	http.HandleFunc("/public-key", handlePublicKey)
+	http.HandleFunc("/webhook", handleWebhook)
+
+	addr := "localhost:4242"
+	log.Printf("Listening on %s ...", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	var req struct {
+		IsBuyingSticker bool `json:"isBuyingSticker"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("json.NewDecoder.Decode: %v", err)
+		return
+	}
+
+	params := &stripe.CheckoutSessionParams{
+		PaymentMethodTypes: stripe.StringSlice([]string{
+			"card",
+		}),
+		SubscriptionData: &stripe.CheckoutSessionSubscriptionDataParams{
+			Items: []*stripe.CheckoutSessionSubscriptionDataItemsParams{
+				&stripe.CheckoutSessionSubscriptionDataItemsParams{
+					Plan: stripe.String(os.Getenv("SUBSCRIPTION_PLAN_ID")),
+				},
+			},
+		},
+		SuccessURL: stripe.String(os.Getenv("DOMAIN") + "/success.html"),
+		CancelURL:  stripe.String(os.Getenv("DOMAIN") + "/cancel.html"),
+	}
+	if req.IsBuyingSticker {
+		params.LineItems = []*stripe.CheckoutSessionLineItemParams{
+			&stripe.CheckoutSessionLineItemParams{
+				Name:     stripe.String("Pasha e-book"),
+				Quantity: stripe.Int64(1),
+				Amount:   stripe.Int64(300),
+				Currency: stripe.String(string(stripe.CurrencyUSD)),
+			},
+		}
+	}
+
+	session, err := session.New(params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("session.New: %v", err)
+		return
+	}
+	writeJSON(w, struct {
+		CheckoutSessionID string `json:"checkoutSessionId"`
+	}{
+		CheckoutSessionID: session.ID,
+	})
+}
+
+func handlePublicKey(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	writeJSON(w, struct {
+		PublicKey string `json:"publicKey"`
+	}{
+		PublicKey: os.Getenv("STRIPE_PUBLISHABLE_KEY"),
+	})
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Printf("ioutil.ReadAll: %v", err)
+		return
+	}
+
+	event, err := webhook.ConstructEvent(b, r.Header.Get("Stripe-Signature"), os.Getenv("STRIPE_WEBHOOK_SECRET"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Printf("webhook.ConstructEvent: %v", err)
+		return
+	}
+
+	if event.Type != "checkout.session.completed" {
+		return
+	}
+
+	cust, err := customer.Get(event.GetObjectValue("customer"), nil)
+	if err != nil {
+		log.Printf("customer.Get: %v", err)
+		return
+	}
+
+	if event.GetObjectValue("display_items", "0", "custom") != "" &&
+		event.GetObjectValue("display_items", "0", "custom", "name") == "Pasha e-book" {
+		log.Printf("🔔 Customer is subscribed and bought an e-book! Send the e-book to %s", cust.Email)
+	} else {
+		log.Printf("🔔 Customer is subscribed but did not buy an e-book.")
+	}
+}
+
+func writeJSON(w http.ResponseWriter, v interface{}) {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("json.NewEncoder.Encode: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := io.Copy(w, &buf); err != nil {
+		log.Printf("io.Copy: %v", err)
+		return
+	}
+}
